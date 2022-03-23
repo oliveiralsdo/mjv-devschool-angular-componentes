cc763918785c940472e32936276fe8953b155a3c
Oliveiralsdo <oliveira.lsdo@gmail.com>
2022-03-23 15:04:10 -0300

    first commit

---
+++ src/app/conteudo-principal/conteudo-principal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-conteudo-principal',
+  templateUrl: './conteudo-principal.component.html',
+  styleUrls: ['./conteudo-principal.component.css']
+})
+export class ConteudoPrincipalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
